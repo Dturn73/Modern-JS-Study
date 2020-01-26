4bcbab03ec300de3b35d201e5ff258109b7b356b
David A. Turner <dturn73@gmail.com>
2020-01-26 11:16:39 -0800

    Inital Commit

---
+++ sandbox/app.js
@@ -0,0 +1,19 @@
+document.querySelector('.card-title').addEventListener('click',
+function(){
+  console.log('card title');
+});
+
+document.querySelector('.card-content').addEventListener('click',
+function(){
+  console.log('card content');
+});
+
+document.querySelector('.card').addEventListener('click',
+function(){
+  console.log('card');
+});
+
+document.querySelector('.col').addEventListener('click',
+function(){
+  console.log('col');
+});
